4a1ac8abcd87867408a9901c218a83a308bf87c6
Oontsk <hunterk0212@gmail.com>
2018-08-03 01:08:36 -0400

    a few updates

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -2,7 +2,7 @@ public class BinarySearchTree<E> extends Tree<E> {
 
     protected class BinarySearchTreeNode extends TreeNode {
 
-        protected BinarySearchTree(E data, TreeNode parent) {
+        protected BinarySearchTreeNode(E data, TreeNode parent) {
             super(data, parent);
         }
 
@@ -26,6 +26,7 @@ public class BinarySearchTree<E> extends Tree<E> {
             }
         }
 
+
         protected void setRight(BinarySearchTreeNode setTo) {
             super.children[1] = setTo;
         }
@@ -34,7 +35,7 @@ public class BinarySearchTree<E> extends Tree<E> {
            throw new UnsupportedOperationException();
         }
 
-        public TreeNode delete(E data) {
+        public TreeNode remove(E data) {
             throw new UnsupportedOperationException();
         }
 
@@ -43,5 +44,75 @@ public class BinarySearchTree<E> extends Tree<E> {
         }
 
     }
+
+    protected void rotate(BinarySearchTreeNode guy) {
+        BinarySearchTreeNode par;
+        try {
+            par = (BinarySearchTreeNode) guy.parent;
+        } catch (ClassCastException | NullPointerException e) {
+            par = null;
+        }
+
+        if (par != null) {
+            BinarySearchTreeNode parsLeft = par.left(),
+                    parsRight = par.right();
+            if (parsLeft != null && parsLeft == guy) {
+                BinarySearchTreeNode guysRight = guy.right();
+                par.setLeft(guysRight);
+                if (guysRight != null) {
+                    guysRight.parent = par;
+                }
+                guy.setRight(par);
+            } else {
+                BinarySearchTreeNode guysLeft = guy.left();
+                par.setRight(guysLeft);
+                if (guysLeft != null) {
+                    guysLeft.parent = par;
+                }
+                guy.setLeft(par);
+            }
+            BinarySearchTreeNode giisan;
+            try {
+                giisan = (BinarySearchTreeNode) par.parent;
+            } catch (ClassCastException | NullPointerException e) {
+                giisan = null;
+            }
+
+            if (giisan != null) {
+                BinarySearchTreeNode giisansLeft = giisan.left();
+                if (giisansLeft != null && giisansLeft == par) {
+                    giisan.setLeft(guy);
+                } else {
+                    giisan.setRight(guy);
+                }
+                guy.parent = giisan;
+                par.parent = guy;
+            }
+
+            if (par == root) {
+                root = guy;
+            }
+        }
+    }
+
+    public TreeNode insert(E data) {
+        BinarySearchTreeNode cur;
+        try {
+            cur = (BinarySearchTreeNode) root;
+        } catch (ClassCastException e) {
+            cur = null;
+        }
+
+        if (cur != null) {
+            int res = super.compare(data, cur.data);
+            (if res > 0) {
+
+            }
+
+        }
+
+
+    }
+
 
 }
